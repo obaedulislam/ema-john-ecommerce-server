d308e3e87b224ef902a2ec85e3f1062de0eee25f
obayedul islam <obaed.cse@gmail.com>
2022-11-05 02:21:59 +0600

    MongoDb Connection establish

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,13 @@ const port = process.env.PORT || 5000;
 //MiddleWare
 app.use(cors());
 app.use(express.json());
+
+
+//MongoDb Connection wit Server
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ypkp6ia.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 app.get('/', (req, res) => {
     res.send("Ema John Server is Running");
